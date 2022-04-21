353ed749741b9427cea715d92e94e08e16f71157
Denise Nordfalt <denisenordfalt@gmail.com>
2022-04-21 13:43:46 +0200

    testing routes for heroku

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { user_login, user_update, verify, create_user, getUser } = require("../controllers/userController");
-const router = express.Router()
-router.post("/users/login", user_login)
-router.put("/user", verify, user_update)
-router.post("/users", create_user)
-router.get("/user", verify, getUser)
-module.exports = router
+const {
+  user_login,
+  user_update,
+  verify,
+  create_user,
+  getUser,
+} = require("../controllers/userController");
+const router = express.Router();
+// router.post("/users/login", user_login)
+// router.put("/user", verify, user_update)
+// router.post("/users", create_user)
+// router.get("/user", verify, getUser)
+router.post("/login", user_login);
+router.put("/", verify, user_update);
+router.post("/", create_user);
+router.get("/", verify, getUser);
+module.exports = router;
